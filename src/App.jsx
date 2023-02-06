7e18cccde66c30c5ae47bd97ab15b3778a5519d9
Ammar Kashif <ammarkashif00@gmail.com>
2023-02-06 09:21:48 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import "@picocss/pico/css/pico.css";
+
+function App() {
+    return (
+        <main className="container">
+            <div className="headings">
+                <h1>Fast Time Table 📆</h1>
+                <h2>Coming Soon!</h2>
+            </div>
+            <select>
+                <option value="" disabled selected>
+                    Select
+                </option>
+                <option>Op1</option>
+            </select>
+        </main>
+    );
+}
+
+export default App;
